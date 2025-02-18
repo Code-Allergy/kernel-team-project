d1a1259ade1be4624e4051ab2ed8338301d38461
Uche <uchechukwu.udenze@usask.ca>
2025-02-17 20:12:25 -0600

    Created the IVT

--- boot/vectors.h
+++ boot/vectors.h
@@ -1,1 +1,12 @@
-// IVT function prototypes
+#ifndef VECTORS_H
+#define VECTORS_H
+
+void reset_handler(void);
+void undefined_handler(void);
+void svc_handler(void);
+void prefetch_abort_handler(void);
+void data_abort_handler(void);
+void irq_handler(void);
+void fiq_handler(void);
+
+#endif  /* VECTORS_H */
