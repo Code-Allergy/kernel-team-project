c55b9df8a3b26310147e719ef2a24f2345e02ec6
kuang <snowy-shadow@github.com>
2025-02-20 23:07:08 -0600

    uart enabled

--- boot/bootloader.c
+++ boot/bootloader.c
@@ -26,25 +26,24 @@ static inline void gpio_test()
     GpioSetPinMode(GPIO1_BASE, 0xf << 21, GpioPinOut);
     GPIO_set(GPIO1_BASE, 1 << 21);
     // 8N1
-    /* uart_init( */
-    /*     0,         // UART index (0 = UART0, 1 = UART1, etc.) */
-    /*     115200,    // Baud rate for communication */
-    /*     1,         // Stop bit enable (1 = enabled, 0 = disabled) */
-    /*     0,         // Number of stop bits (0 = 1 stop bit, 1 = 1.5/2 stop bits) */
-    /*     0,         // Parity enable (1 = enabled, 0 = disabled) */
-    /*     0,         // Parity type (0 = even, 1 = odd; ignored if parity is disabled) */
-    /*     8          // Character length */
-    /* ); */
+    uart_init(0,      // UART index (0 = UART0, 1 = UART1, etc.)
+              115200, // Baud rate for communication
+              1,      // Stop bit enable (1 = enabled, 0 = disabled)
+              0,      // Number of stop bits (0 = 1 stop bit, 1 = 1.5/2 stop bits)
+              0,      // Parity enable (1 = enabled, 0 = disabled)
+              0,      // Parity type (0 = even, 1 = odd; ignored if parity is disabled)
+              8       // Character length
+    );
 
     /* uart_puts("Sup bro\n"); */
 
     while (1)
     {
         GPIO_set(gpio_base, LED_PINS);
-        /* uart_puts("LEDs on!\n"); */
+        uart_puts("LEDs on!\n");
         delay(0x1FFFFFF);
         GPIO_clear(gpio_base, LED_PINS);
-        /* uart_puts("LEDs off!\n"); */
+        uart_puts("LEDs off!\n");
         delay(0x1FFFFFF);
     }
 }
