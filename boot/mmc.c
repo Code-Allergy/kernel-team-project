49e73693e660b4e3ca794e85b9fd2cb46cd630ae
Devam <dns682@usask.ca>
2025-03-14 01:31:22 -0600

    refactored a bit

--- boot/mmc.c
+++ boot/mmc.c
@@ -158,6 +158,173 @@ int sendCommand(unsigned int cmdNum, unsigned int args, unsigned int flags,
     return CC;
 }
 
+int enableClocks(void)
+{
+    unsigned int timeout;
+
+    /*writing to CM_PER_MMC0_CLKCTRL enables SD OCP clock and CLKADPI clock*/
+    REG32_write_masked(CM_PER_BASE, CM_PER_MMC0_CLKCTRL, 0b11, 0x2);
+    timeout = 100000;
+
+    /*wait until fully enabled*/
+    while (REG32_read_masked(CM_PER_BASE, CM_PER_MMC0_CLKCTRL, (0b11 << 16)) 
+            != 0x0)
+    {
+        if (--timeout == 0)
+        {
+            uart_puts("Timeout waiting for MMC module clock\n");
+            return -1;
+        }
+    }
+    return 0;
+}
+
+int enableDeBounceClock(void)
+{
+    unsigned int timeout;
+
+    /*we first need to enable the debounce clock in order to monitor if
+     * software reset is done*/
+    REG32_write_masked(CONTROL_MODULE_BASE, CLK32KDIVRATIO_CTRL, 0b1, 0b1);
+    timeout = 100000;
+    while(REG32_read_masked(CONTROL_MODULE_BASE, CLK32KDIVRATIO_CTRL, 0b1) 
+            != 0b1)
+    {
+        if (--timeout == 0)
+        {
+            uart_puts("Timeout waiting for de bounce clock\n");
+            return -1;
+        }
+    }
+    return 0;
+}
+
+
+int softwareReset(void)
+{
+    unsigned int timeout;
+
+    /*software reset*/
+    REG32_write_masked(MMCHS0_BASE, SD_SYSCONFIG, 0x2, 0x2); /*write 1 to 
+    the SOFTRESET (1st) bit*/
+
+    /*wait for the request to complete*/
+    timeout = 100000;
+    while(REG32_read_masked(MMCHS0_BASE, SD_SYSSTATUS, 0x1) != 0x1)
+    {
+        if (--timeout == 0)
+        {
+            uart_puts("Timeout waiting for MMC SOFTRESET\n");
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+int linesReset(void)
+{
+    unsigned int timeout;
+
+    uart_puts("waiting for lines reset\n");
+
+    REG32_write_masked(MMCHS0_BASE, SD_SYSCTL, (0b1 << 24), (0b1 << 24));
+   
+    timeout = 100000;
+    while(REG32_read_masked(MMCHS0_BASE, SD_SYSCTL, (0b1 << 24)) != 0)
+    {
+        if (--timeout == 0)
+        {
+            uart_puts("Timeout waiting for lines reset\n");
+            return -1;
+        }
+    }
+    return 0;
+}
+
+
+void setBusSupportedVoltage(void)
+{
+    /*support 3.3 voltage which is what beaglebone black supports according to
+     *its documentation*/
+    REG32_write_masked(MMCHS0_BASE, SD_CAPA, (0b111 << 24), (0b001 << 24));
+    /*mask to access 26th 25th and 24th bit and writing 0 0 1
+     *because we only support 3.3v not 1.8 or 3.0*/
+    
+    /*write max current 0 to register SD_CUR_CAPA for all 3 voltage*/
+    REG32_write_masked(MMCHS0_BASE, SD_CUR_CAPA, (0b111111111111111111111111),
+            0);
+}
+
+/*
+ * 0x7 for 3.3v
+ * */
+void setBusVoltage(unsigned int voltage)
+{
+    /*SDVS bits 9-11 = 7h for 3.3v*/
+    REG32_write_masked(MMCHS0_BASE, SD_HCTL, (0b111 << 9), (voltage << 9));
+}
+
+
+void setBusWidth(void)
+{
+    /*set bus width to 1- bit*/
+    REG32_write_masked(MMCHS0_BASE, SD_HCTL, (0b1 << 1), 0);
+}
+
+int setBusPowerOn(void)
+{
+    unsigned int timeout;
+
+    /*SDBP bit 8 1h = bus power on*/
+    REG32_write_masked(MMCHS0_BASE, SD_HCTL, (0b1 << 8), (0x1 << 8));
+
+    timeout = 100000000;
+    while(REG32_read_masked(MMCHS0_BASE, SD_HCTL, (0b1 << 8)) != (0x1 << 8))
+    {
+        if (--timeout == 0)
+        {
+            uart_puts("Timeout waiting for SD bus power on\n");
+            return -1;
+        }
+    }
+    return 0;
+}
+
+int enableInternalClock(unsigned int divider)
+{
+    unsigned int timeout;
+
+    if(divider == 0 || divider == 1 || divider > 1023)
+    {
+        uart_puts("not supported frequency\n");
+        return -1;
+    }
+
+    /*enable the internal clock*/
+    REG32_write_masked(MMCHS0_BASE, SD_SYSCTL, (0b1), (0x1));
+
+    REG32_write_masked(MMCHS0_BASE, SD_SYSCTL, (0b1111111111 << 6), 
+            (divider << 6));
+
+    /*wait for clock to stabilize by reading ICS bit of SD_SYSCTL*/
+    timeout = 100000;
+    while(REG32_read_masked(MMCHS0_BASE, SD_SYSCTL, (0b1 << 1)) != (0x1 << 1))
+    {
+        if (--timeout == 0)
+        {
+            uart_puts("Timeout waiting for MMC clock to be stable\n");
+            return -1;
+        }
+    }
+    return 0;
+}
+
+void sendInitStream(void)
+{
+    /*send initilization stream*/
+    REG32_write_masked(MMCHS0_BASE, SD_CON, (0b1 << 1), (0x1 << 1));
+}
 
 void mmc_controller_init(void)
 {
@@ -181,74 +348,35 @@ void mmc_controller_init(void)
     /*enable clocks*/ 
     uart_puts("trying to enable interface and functional clocks\n");
 
-    /*writing to CM_PER_MMC0_CLKCTRL enables SD OCP clock and CLKADPI clock*/
-    REG32_write_masked(CM_PER_BASE, CM_PER_MMC0_CLKCTRL, 0b11, 0x2);
-    timeout = 100000;
-
-    /*wait until fully enabled*/
-    while (REG32_read_masked(CM_PER_BASE, CM_PER_MMC0_CLKCTRL, (0b11 << 16)) 
-            != 0x0)
+    if(enableClocks() != 0)
     {
-        if (--timeout == 0)
-        {
-            uart_puts("Timeout waiting for MMC module clock\n");
-            return;
-        }
+        return;
     }
 
     uart_puts("Successfully enabled interface and functional clocks\n");
     
-    /*we first need to enable the debounce clock in order to monitor if
-     * software reset is done*/
-    REG32_write_masked(CONTROL_MODULE_BASE, CLK32KDIVRATIO_CTRL, 0b1, 0b1);
-    timeout = 100000;
-    while(REG32_read_masked(CONTROL_MODULE_BASE, CLK32KDIVRATIO_CTRL, 0b1) 
-            != 0b1)
+    if(enableDeBounceClock() != 0)
     {
-        if (--timeout == 0)
-        {
-            uart_puts("Timeout waiting for de bounce clock\n");
-            return;
-        }
+        return;
     }
-    
+
     uart_puts("Successfully enabled debounce clock\n");
 
-    /*software reset*/
-    REG32_write_masked(MMCHS0_BASE, SD_SYSCONFIG, 0x2, 0x2); /*write 1 to 
-    the SOFTRESET (1st) bit*/
-
-    /*wait for the request to complete*/
-    timeout = 100000;
-    while(REG32_read_masked(MMCHS0_BASE, SD_SYSSTATUS, 0x1) != 0x1)
+    if(softwareReset() != 0)
     {
-        if (--timeout == 0)
-        {
-            uart_puts("Timeout waiting for MMC SOFTRESET\n");
-            return;
-        }
+        return;
     }
-    uart_puts("waiting for lines reset\n");
 
-    REG32_write_masked(MMCHS0_BASE, SD_SYSCTL, (0b1 << 24), (0b1 << 24));
-    
-    while(REG32_read_masked(MMCHS0_BASE, SD_SYSCTL, (0b1 << 24)) != 0);
+    if(linesReset() != 0)
+    {
+        return;
+    }
 
     uart_puts("lines reset complete\n");
 
     uart_puts("SOFTRESET complete\n");
 
     /*Set modules hardware capabilitites*/
-
-    /*support 3.3 voltage which is what beaglebone black supports according to
-     *its documentation*/
-    REG32_write_masked(MMCHS0_BASE, SD_CAPA, (0b111 << 24), (0b001 << 24));
-    /*mask to access 26th 25th and 24th bit and writing 0 0 1
-     *because we only support 3.3v not 1.8 or 3.0*/
-    
-    /*write max current 0 to register SD_CUR_CAPA for all 3 voltage*/
-    REG32_write_masked(MMCHS0_BASE, SD_CUR_CAPA, (0b111111111111111111111111),
-            0);
 
     /*MMC Host and Bus Configuration*/
 
@@ -262,52 +390,25 @@ void mmc_controller_init(void)
     /*configure SD_CON to Standard MMC/SD/SDIO mode*/
     REG32_write_masked(MMCHS0_BASE, SD_CON, (0b1 << 12), 0);
 
-    /*SD_HCTL register:*/
 
-    /*SDVS bits 9-11 = 7h for 3.3v*/
-    REG32_write_masked(MMCHS0_BASE, SD_HCTL, (0b111 << 9), (0x7 << 9));
+    setBusSupportedVoltage();
+
+    /*0x7 for 3.3v*/
+    setBusVoltage(0x7);
     
-    /*SDBP bit 8 1h = bus power on*/
-    REG32_write_masked(MMCHS0_BASE, SD_HCTL, (0b1 << 8), (0x1 << 8));
-
-    /*set bus width to 1- bit*/
-    REG32_write_masked(MMCHS0_BASE, SD_HCTL, (0b1 << 1), 0);
-
     uart_puts("trying to power on SD bus\n");
-
-    timeout = 100000000;
-    while(REG32_read_masked(MMCHS0_BASE, SD_HCTL, (0b1 << 8)) != (0x1 << 8))
-    {
-        if (--timeout == 0)
-        {
-            uart_puts("Timeout waiting for SD bus power on\n");
-            return;
-        }
-    }
     
+    if(setBusPowerOn() != 0)
+    {
+        return;
+    }
+
     uart_puts("set voltage is supported\n");
 
-    /*SD_SYSCTL register:*/
 
-    /*enable the internal clock*/
-    REG32_write_masked(MMCHS0_BASE, SD_SYSCTL, (0b1), (0x1));
-
-    /*CLKD bits 6-15 set divider to set clock freq to 80KHz from 96000KHz*/
-    divider = 48000 / 80;   /*we want to set to 80 from 96MHz = 96000KHz = 
-                              600*/
-
-    REG32_write_masked(MMCHS0_BASE, SD_SYSCTL, (0b1111111111 << 6), 
-            (divider << 6));
-
-    /*wait for clock to stabilize by reading ICS bit of SD_SYSCTL*/
-    timeout = 100000;
-    while(REG32_read_masked(MMCHS0_BASE, SD_SYSCTL, (0b1 << 1)) != (0x1 << 1))
+    if(enableInternalClock(96000 / 400) != 0)
     {
-        if (--timeout == 0)
-        {
-            uart_puts("Timeout waiting for MMC clock to be stable\n");
-            return;
-        }
+        return;
     }
 
     uart_puts("MMC clock stable after setting divider\n");
@@ -327,9 +428,8 @@ void mmc_controller_init(void)
     uart_puts("MMC module init complete\n");
 
     /*Card Detection, Identification, and Selection*/
-
-    /*send initilization stream*/
-    REG32_write_masked(MMCHS0_BASE, SD_CON, (0b1 << 1), (0x1 << 1));
+    
+    sendInitStream();
 
     /*write 0x00000000 in the SD_CMD register*/
     REG32_write(MMCHS0_BASE, SD_CMD, 0x00000000);
@@ -366,35 +466,40 @@ void mmc_controller_init(void)
     uart_puts("setting clock high\n");
     
     /*disable the internal clock*/
-    REG32_write_masked(MMCHS0_BASE, SD_SYSCTL, 0b1, 0x0);
- 
+    /*
+     REG32_write_masked(MMCHS0_BASE, SD_SYSCTL, 0b1, 0x0);
+     */
+
     /*Change clock frequency to fit protocol*/
     /*CLKD bits 6-15 set divider to 1 to set clock freq to 24MHz from 
      * default 48000KHz*/
     
-    divider = 2;    /*or 2*/
+    /*divider = 96000 / 400;*/    /*or 2*/
 
-    
+    /*
     REG32_write_masked(MMCHS0_BASE, SD_SYSCTL, (0b1111111111 << 6), 
             (divider << 6));
-
+    */
     /*enable the internal clock*/
+    
+    /*
     REG32_write_masked(MMCHS0_BASE, SD_SYSCTL, 0b1, 0x1);
     
     uart_puts("waiting for clock to be stable\n");
-    
+    */
+
     /*wait for clock to stabilize by reading ICS bit of SD_SYSCTL*/
+    /*
     timeout = 100000;
     while(REG32_read_masked(MMCHS0_BASE, SD_SYSCTL, (0b1 << 1)) != (0x1 << 1))
     {
-        uart_puts("internal clock was not stable\n");
         if (--timeout == 0)
         {
             uart_puts("Timeout waiting for MMC clock to be stable\n");
             return;
         }
     }
-    
+    */
     uart_puts("now need to send commands\n");
     /*Send a CMD0 command*/
 
