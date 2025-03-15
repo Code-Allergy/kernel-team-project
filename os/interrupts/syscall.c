0bef03d8a7172c42379fa3ed7e1b0b9b99d87786
Bristine Teneng <bristine.teneng@usask.ca>
2025-03-15 07:55:19 -0600

    Basic syscall table setup

--- os/interrupts/syscall.c
+++ os/interrupts/syscall.c
@@ -1,24 +1,10 @@
 #include <syscall.h>
 #include <errno.h>
-#include <gpio.h>
-
-typedef int (*syscall_func)(va_list args);
-syscall_func syscall_vector_table[NUM_SYSCALLS];
-#define __SYSCALL__(n, swi_routine) \
-                                    syscall_vector_table[n] = swi_routine \
-
-void setup_syscall_table(void){
-    __SYSCALL__(SYS_DEFAULT, sys_default);
-    __SYSCALL__(SYS_READ, sys_read);
-    __SYSCALL__(SYS_WRITE, sys_write);
-}
-
 
 int handle_syscall(uint16_t syscall_num, ...){
     va_list args;
     int result;
 
-    GPIO_set(GPIO1_BASE, LED2);
     if (syscall_num > NUM_SYSCALLS){
         return (-ENOSYSCALL);
     }
@@ -39,24 +25,16 @@ int sys_default(va_list args){
 int sys_read(va_list args){
     uint8_t fd;
     char* buffer;
-    uint32_t len;
+    int16_t len;
+    SYSCALL_ARGS3(uint8_t, fd, char*, buffer, int16_t, len);
 
-    fd = (uint8_t)va_arg(args, int);
-    buffer = va_arg(args, char*);
-    len = (uint32_t)va_arg(args, int);
-    va_end(args);
-
-    GPIO_set(GPIO1_BASE, LED3);
     return 0;
 }
 int sys_write(va_list args){
     uint8_t fd;
     char* buffer;
-    uint32_t len;
-
-    fd = (uint8_t)va_arg(args, int);
-    buffer = va_arg(args, char*);
-    len = (uint32_t)va_arg(args, int);
+    int16_t len;
+    SYSCALL_ARGS3(uint8_t, fd, char*, buffer, int16_t, len);
 
     return 0;
 }
--- os/interrupts/syscall.h
+++ os/interrupts/syscall.h
@@ -3,6 +3,60 @@
 
 #include <types.h>
 
+#define __SYSCALL__(n, swi_routine) \
+                                    syscall_vector_table[n] = swi_routine \
+
+#define SYSCALL_ARGS1(type0, arg0)  \
+    do{ \
+        arg0 = va_arg(args, type0); \
+        va_end(args); \
+    }while(0)
+
+#define SYSCALL_ARGS2(type0, arg0, type1, arg1)  \
+    do{ \
+        arg0 = va_arg(args, type0); \
+        arg1 = va_arg(args, type1); \
+        va_end(args); \
+    }while(0)
+
+#define SYSCALL_ARGS3(type0, arg0, type1, arg1, type2, arg2)  \
+    do{ \
+        arg0 = va_arg(args, type0); \
+        arg1 = va_arg(args, type1); \
+        arg2 = va_arg(args, type2); \
+        va_end(args); \
+    }while(0)
+
+#define SYSCALL_ARGS4(type0, arg0, type1, arg1, type2, arg2, type3, arg3)  \
+    do{ \
+        arg0 = va_arg(args, type0); \
+        arg1 = va_arg(args, type1); \
+        arg2 = va_arg(args, type2); \
+        arg3 = va_arg(args, type3); \
+        va_end(args); \
+    }while(0)
+
+#define SYSCALL_ARGS5(type0, arg0, type1, arg1, type2, arg2, type3, arg3, type4, arg4)  \
+    do{ \
+        arg0 = va_arg(args, type0); \
+        arg1 = va_arg(args, type1); \
+        arg2 = va_arg(args, type2); \
+        arg3 = va_arg(args, type3); \
+        arg4 = va_arg(args, type4); \
+        va_end(args); \
+    }while(0)
+
+#define SYSCALL_ARGS6(type0, arg0, type1, arg1, type2, arg2, type3, arg3, type4, arg4, type5, arg5)  \
+    do{ \
+        arg0 = va_arg(args, type0); \
+        arg1 = va_arg(args, type1); \
+        arg2 = va_arg(args, type2); \
+        arg3 = va_arg(args, type3); \
+        arg4 = va_arg(args, type4); \
+        arg5 = va_arg(args, type5); \
+        va_end(args); \
+    }while(0)
+
 #define SYS_DEFAULT         0
 #define SYS_READ            1
 #define SYS_WRITE           3
@@ -17,11 +71,15 @@ int sys_read(va_list args);
 int sys_write(va_list args);
 
 
+typedef int (*syscall_func)(va_list args);
+syscall_func syscall_vector_table[NUM_SYSCALLS];
 /* Register routine in syscall table */
-/* Populate this in syscall.c */
-void setup_syscall_table(void);
+void setup_syscall_table(void){
+__SYSCALL__(SYS_DEFAULT, sys_default);
+__SYSCALL__(SYS_READ, sys_read);
+__SYSCALL__(SYS_WRITE, sys_write);
+}
 
-extern int syscall(uint16_t syscall_num, ...);
-int handle_syscall(uint16_t syscall_num, ...);
+int handle_syscall(int syscall_num, ...);
 
 #endif  /*__SYSCALL_H */
