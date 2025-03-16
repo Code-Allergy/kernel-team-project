cd5a93b76755be83415dd3728c028c6168f4093d
Wahhaj Javed <wahhajjaved@gmail.com>
2025-03-16 17:18:26 -0600

    write implemted. compiles but not tested

--- os/drivers/motor.c
+++ os/drivers/motor.c
@@ -1,19 +1,44 @@
 //  Motor control driver
 
 #include <motor.h>
+#include <gpio.h>
 
 motor_t motors[NUM_MOTORS];
 
+/*
+ * Reads the current state of the motor
+ */
 int motor_read(int motor_num, void* buf, int count) {
-	return -1;
-
-
+	if (motor_num < 0 || motor_num > NUM_MOTORS)
+		return -1;
 }
 
-
+/*
+ * Negative means go backwards. Positive means go forwards
+ * 0 means stop
+ */
 int motor_write(int motor_num, void* buf, int count) {
-	return -1;
+	motor_t motor;
+	int v;
 
+	if (motor_num < 0 || motor_num > NUM_MOTORS)
+		return -1;
+
+	motor = motors[motor_num];
+	v = *((int*)buf);
+	if (v == 0) {
+		GPIO_clear(motor.gpio_base, motor.forwards_pin);
+		GPIO_clear(motor.gpio_base, motor.backwards_pin);
+		return 0;
+	}
+	else if (v > 0) {
+		GPIO_set(motor.gpio_base, motor.forwards_pin);
+		return 0;
+	}
+	else {
+		GPIO_set(motor.gpio_base, motor.backwards_pin);
+		return 0;
+	}
 }
 
 
--- os/drivers/motor.h
+++ os/drivers/motor.h
@@ -1,12 +1,19 @@
 //  Motor control API
+#ifndef MOTOR_H
+#define MOTOR_H
+
+#include <gpio.h>
 
 #define NUM_MOTORS 4
 
 typedef struct {
 	int motor_number;
+	enum GpioIOBase gpio_base;
 	int forwards_pin;
 	int backwards_pin;
 } motor_t;
 
 
+
+#endif //__MOTOR_H__
 
