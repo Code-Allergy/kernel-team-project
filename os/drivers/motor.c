fea5b482ce78006fad6609d10a12c81986602b93
Wahhaj Javed <wahhajjaved@gmail.com>
2025-03-16 16:45:58 -0600

    stubs for motor driver

--- os/drivers/motor.c
+++ os/drivers/motor.c
@@ -1,1 +1,19 @@
 //  Motor control driver
+
+#include <motor.h>
+
+motor_t motors[NUM_MOTORS];
+
+int motor_read(int motor_num, void* buf, int count) {
+	return -1;
+
+
+}
+
+
+int motor_write(int motor_num, void* buf, int count) {
+	return -1;
+
+}
+
+
