04929c11c493121f8ea98d9c29a7794d047583a4
Wahhaj Javed <muj975@usask.ca>
2025-03-20 17:22:17 -0600

    replaced motor forwards and backwards pins with pin1 and pin2

--- os/drivers/motor.h
+++ os/drivers/motor.h
@@ -6,14 +6,16 @@
 
 #define NUM_MOTORS 4
 
+/* to go forward, (pin1, pin2) = (0, 1)
+ * to go back, (pin1, pin2) = (1, 0)
+ * to stop (pin1, pin2) = (0, 0) or (1, 1)
+ */
 typedef struct {
 	int motor_number;
 	enum GpioIOBase gpio_base;
-	int forwards_pin;
-	int backwards_pin;
+	int pin1;
+	int pin2;
 } motor_t;
-
-
 
 #endif //__MOTOR_H__
 
